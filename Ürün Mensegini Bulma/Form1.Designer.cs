d09c9c7f7824910b3426c719d70672f541488f11
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2018-11-11 03:15:56 +0300

    Add project files.

---
+++ Ürün Mensegini Bulma/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Ürün_Mensegini_Bulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ribbonControl1 = new DevComponents.DotNetBar.RibbonControl();
+            this.ribbonClientPanel1 = new DevComponents.DotNetBar.Ribbon.RibbonClientPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.ribbonClientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.CaptionVisible = true;
+            this.ribbonControl1.DefaultGroupFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ribbonControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ribbonControl1.KeyTipsFont = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.Office2007ColorTable = DevComponents.DotNetBar.Rendering.eOffice2007ColorScheme.Black;
+            this.ribbonControl1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
+            this.ribbonControl1.Size = new System.Drawing.Size(769, 28);
+            this.ribbonControl1.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.ribbonControl1.SystemText.QatDialogAddButton = "&Add >";
+            this.ribbonControl1.SystemText.QatDialogCancelButton = "Geri";
+            this.ribbonControl1.TabGroupHeight = 14;
+            this.ribbonControl1.TabIndex = 35;
+            this.ribbonControl1.Text = "ribbonControl1";
+            this.ribbonControl1.TitleText = "Firma Menşeği Bulma";
+            // 
+            // ribbonClientPanel1
+            // 
+            this.ribbonClientPanel1.CanvasColor = System.Drawing.SystemColors.Control;
+            this.ribbonClientPanel1.Controls.Add(this.richTextBox1);
+            this.ribbonClientPanel1.Controls.Add(this.textBoxX1);
+            this.ribbonClientPanel1.Controls.Add(this.buttonX1);
+            this.ribbonClientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ribbonClientPanel1.Location = new System.Drawing.Point(0, 28);
+            this.ribbonClientPanel1.Name = "ribbonClientPanel1";
+            this.ribbonClientPanel1.Size = new System.Drawing.Size(769, 412);
+            // 
+            // 
+            // 
+            this.ribbonClientPanel1.Style.Class = "RibbonClientPanel";
+            this.ribbonClientPanel1.TabIndex = 39;
+            this.ribbonClientPanel1.Text = "ribbonClientPanel1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(45, 166);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(656, 175);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.Class = "TextBoxBorder";
+            this.textBoxX1.Location = new System.Drawing.Point(278, 62);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(118, 20);
+            this.textBoxX1.TabIndex = 1;
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(285, 109);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(103, 36);
+            this.buttonX1.TabIndex = 0;
+            this.buttonX1.Text = "Ara";
+            this.buttonX1.Click += new System.EventHandler(this.buttonX1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 440);
+            this.Controls.Add(this.ribbonClientPanel1);
+            this.Controls.Add(this.ribbonControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ribbonClientPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevComponents.DotNetBar.RibbonControl ribbonControl1;
+        private DevComponents.DotNetBar.Ribbon.RibbonClientPanel ribbonClientPanel1;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Ürün Mensegini Bulma/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ürün_Mensegini_Bulma.svr;
+
+
+namespace Ürün_Mensegini_Bulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        AndroidSoapClient client = new AndroidSoapClient();
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            var sss = client.SearchByGTIN("GepirMobile", "g!2*14Cm%s", "", "", textBoxX1.Text);
+            for (int i = 0; i < sss.Count; i++)
+            {
+               richTextBox1.Text = sss[i]; 
+            }
+           
+
+        }
+    }
+}
